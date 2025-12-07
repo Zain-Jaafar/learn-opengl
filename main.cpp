fe94db5c524a8c5f7307806a483ca8e45d3194e4
Zain Jaafar <zainjaafar120@gmail.com>
2025-12-08 11:55:23 +1300

    2 triangles side by side

--- main.cpp
+++ main.cpp
@@ -47,14 +47,15 @@ int main(int argc, char* argv[]) {
     
     // Vertices for the triangles
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+        -0.5f,   0.5f, 0.0f,  // left top
+        -0.75f, -0.5f, 0.0f,  // left bottom left
+         0.0f,  -0.5f, 0.0f,  // bottom middle
+         0.5f,   0.5f, 0.0f,  // right top 
+         0.75f, -0.5f, 0.0f   // right bottom right
     };
     unsigned int indices[] = {
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+        0, 1, 2,   // first triangle
+        2, 3, 4    // second triangle
     };
 
     // Create Buffers
