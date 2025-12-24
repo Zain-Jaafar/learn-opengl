fa5201556ab21e1202cc66edd87879980515998c
Zain Jaafar <zainjaafar120@gmail.com>
2025-12-24 22:35:24 +1300

    Added DearImGUI

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+
 #include <iostream>
 
 #include <SDL2/SDL.h>
@@ -14,6 +18,10 @@
 
 
 int main(int argc, char* argv[]) {
+
+    const int screenWidth = 1600;
+    const int screenHeight = 900;
+
     // Initialise SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "SDL could not initialize! " << SDL_GetError() << "\n";
@@ -27,8 +35,9 @@ int main(int argc, char* argv[]) {
 
     // Create window
     SDL_Window* window = SDL_CreateWindow("Learning OpenGL",
-                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                          800, 600,
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          SDL_WINDOWPOS_CENTERED,
+                                          screenWidth, screenHeight,
                                           SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     if (!window) {
         std::cerr << "Window creation failed: " << SDL_GetError() << "\n";
@@ -137,6 +146,7 @@ int main(int argc, char* argv[]) {
     Shader lampShader("lampShader.vs", "lampShader.fs");
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glViewport(0, 0, screenWidth, screenHeight);
 
     float deltaTime = 0.0f;
     float lastFrame = 0.0f;
@@ -145,11 +155,24 @@ int main(int argc, char* argv[]) {
     glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
     glm::vec3 objectColor = glm::vec3(1.0f, 0.5f, 0.3f);
 
+    float ambientStrength = 0.15f;
+    float specularStrength = 0.5f;
+    int specularIntensity = 64;
+
+    SDL_bool mouseLocked = SDL_TRUE;
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_InitForOpenGL(window, context);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
     // Main program loop
     bool running = true;
     while (running) {
         // Capture and hide mouse cursor
-        SDL_SetRelativeMouseMode(SDL_TRUE);
+        SDL_SetRelativeMouseMode(mouseLocked);
 
         float currentFrame = SDL_GetPerformanceCounter();
         deltaTime = (currentFrame - lastFrame)*1000 / SDL_GetPerformanceFrequency();
@@ -167,6 +190,14 @@ int main(int argc, char* argv[]) {
                     std::cout << "ESC key pressed" << "\n";
                     running = false;
                 }
+                if (event.key.keysym.sym == SDLK_COMMA) {
+                    std::cout << "COMMA pressed" << "\n";
+                    if (mouseLocked == SDL_TRUE) {
+                        mouseLocked = SDL_FALSE;
+                    } else {
+                        mouseLocked = SDL_TRUE;
+                    }
+                }
 
                 if (event.key.keysym.sym == SDLK_w) {
                     camera.ProcessKeyboard(FORWARD, deltaTime);
@@ -183,30 +214,43 @@ int main(int argc, char* argv[]) {
             }
 
             if (event.type == SDL_MOUSEMOTION) {
-                int dx = event.motion.xrel;
-                int dy = -event.motion.yrel;
+                if (mouseLocked == SDL_TRUE) {
+                    int dx = event.motion.xrel;
+                    int dy = -event.motion.yrel;
 
-                camera.ProcessMouseMovement(dx, dy);
+                    camera.ProcessMouseMovement(dx, dy);
+                }
             }
 
             if (event.type == SDL_MOUSEWHEEL) {
-                int scrollX = event.wheel.x;
-                int scrollY = event.wheel.y;
-                
-                camera.ProcessMouseScroll(scrollY);
+                if (mouseLocked == SDL_TRUE) {
+                    int scrollX = event.wheel.x;
+                    int scrollY = event.wheel.y;
+                    
+                    camera.ProcessMouseScroll(scrollY);
+                }
             }
+            
+            ImGui_ImplSDL2_ProcessEvent(&event);
         }
 
         // Draw background
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
 
         cubeShader.use();
         cubeShader.setVec3("lightColor", lightColor);
         cubeShader.setVec3("lightPos", lampPos);
         cubeShader.setVec3("objectColor", objectColor);
         cubeShader.setVec3("viewPos", camera.Position);
+
+        cubeShader.setFloat("ambientStrength", ambientStrength);
+        cubeShader.setFloat("specularStrength", specularStrength);
+        cubeShader.setInt("specularIntensity", specularIntensity);
 
         glm::mat4 model = glm::mat4(1.0f);
 
@@ -215,7 +259,7 @@ int main(int argc, char* argv[]) {
         glm::mat4 projection;
         projection = glm::perspective(
           glm::radians(camera.Zoom), 
-          800.0f/600.0f, 
+          (float)screenWidth/(float)screenHeight, 
           0.1f, 
           100.0f
         );
@@ -234,11 +278,9 @@ int main(int argc, char* argv[]) {
         lampShader.setVec3("lightColor", lightColor);
         lampShader.setMat4("projection", projection);
         lampShader.setMat4("view", view);
+
         model = glm::mat4(1.0f);
 
-
-        lampPos.x = 2.0f * sin(SDL_GetTicks64()/1000.f);
-        lampPos.z = 2.0f * cos(SDL_GetTicks64()/1000.f);
         model = glm::translate(model, lampPos);
 
         model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
@@ -249,10 +291,35 @@ int main(int argc, char* argv[]) {
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glBindVertexArray(0);
+      
+        ImGui::Begin("Testing Station");
+        ImGui::Text("Hello there, Adventurer!");
+        ImGui::Text("Colour: ");
+        ImGui::ColorEdit3("Object", (float*)&objectColor);
+        ImGui::ColorEdit3("Lamp", (float*)&lightColor);
+        ImGui::SliderFloat("Ambient Strength", &ambientStrength, 0.0f, 1.0f);
+        ImGui::SliderFloat("Specular Strength", 
+                           &specularStrength, 
+                           0.0f, 1.0f);
+        ImGui::SliderInt("Specular Intensity", 
+                         &specularIntensity, 
+                         0, 1024);
+        ImGui::Text("Lamp Position: ");
+        ImGui::SliderFloat("X", &lampPos.x, -3.0f, 3.0f);
+        ImGui::SliderFloat("Y", &lampPos.y, -3.0f, 3.0f);
+        ImGui::SliderFloat("Z", &lampPos.z, -3.0f, 3.0f);
+        ImGui::End();
+        
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         
         // Draw new frame
         SDL_GL_SwapWindow(window);
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
 
     // Cleanly close the window
     SDL_GL_DeleteContext(context);
