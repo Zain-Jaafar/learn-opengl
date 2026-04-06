8a546e217a0dd9eeb1a3a44720b0c1ef0a650254
Zain Jaafar <zainjaafar120@gmail.com>
2026-04-06 19:35:19 +1200

    Refactor code and add Mesh class

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "imgui_impl_opengl3.h"
 
 #include <iostream>
+#include <string>
 
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
@@ -13,9 +14,86 @@
 
 #include "shader.h"
 #include "camera.h"
-#include "texture.h"
 #include "stb_image.h"
 
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    string type;
+};
+
+class Mesh {
+    public:
+        vector<Vertex>       vertices;
+        vector<unsigned int> indices;
+        vector<Texture>      textures;
+
+        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures) {
+            this->vertices = vertices;
+            this->indices = indices;
+            this->textures = textures;
+
+            setupMesh();
+        }
+        void Draw(Shader &shader) {
+            unsigned int diffuseNr = 1;
+            unsigned int specularNr = 1;
+            for(unsigned int i = 0; i < textures.size(); i++)
+            {
+                glActiveTexture(GL_TEXTURE0 + i); // activate proper texture unit before binding
+                // retrieve texture number (the N in diffuse_textureN)
+                string number;
+                string name = textures[i].type;
+                if(name == "texture_diffuse")
+                    number = std::to_string(diffuseNr++);
+                else if(name == "texture_specular")
+                    number = std::to_string(specularNr++);
+
+                shader.setInt(("material." + name + number).c_str(), i);
+                glBindTexture(GL_TEXTURE_2D, textures[i].id);
+            }
+            glActiveTexture(GL_TEXTURE0);
+
+            // draw mesh
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+            glBindVertexArray(0);
+        } 
+    private:
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh() {
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glGenBuffers(1, &EBO);
+          
+            glBindVertexArray(VAO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);  
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), 
+                         &indices[0], GL_STATIC_DRAW);
+
+            // vertex positions
+            glEnableVertexAttribArray(0);	
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+            // vertex normals
+            glEnableVertexAttribArray(1);	
+            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+            // vertex texture coords
+            glEnableVertexAttribArray(2);	
+            glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+            glBindVertexArray(0);
+        }  
+}
 
 int main(int argc, char* argv[]) {
 
@@ -156,8 +234,61 @@ int main(int argc, char* argv[]) {
     Shader cubeShader("cubeShader.vs", "cubeShader.fs");
     Shader lampShader("lampShader.vs", "lampShader.fs");
 
-    Texture containerTexture("container2.png");
-    Texture containerSpecularMap("container2_specular.png");
+    
+    unsigned int containerTexture;
+    
+    glGenTextures(1, &containerTexture);
+    glBindTexture(GL_TEXTURE_2D, containerTexture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned char *data = stbi_load("container2.png", &width, &height, &nrChannels, 0);
+
+    GLenum format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture: container2.png" << std::endl;
+    }
+
+    stbi_image_free(data);
+
+
+    unsigned int containerSpecularMap;
+    
+    glGenTextures(1, &containerSpecularMap);
+    glBindTexture(GL_TEXTURE_2D, containerSpecularMap);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_set_flip_vertically_on_load(true);
+
+    data = stbi_load("container2_specular.png", &width, &height, &nrChannels, 0);
+
+    format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture: container2_specular.png" << std::endl;
+    }
+
+    stbi_image_free(data);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     glViewport(0, 0, screenWidth, screenHeight);
@@ -291,8 +422,12 @@ int main(int argc, char* argv[]) {
             cubeShader.setFloat(pointLightIndex + "quadratic", 0.032f);
         }
         
-        containerTexture.bind(0);
-        containerSpecularMap.bind(1);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, containerTexture);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, containerSpecularMap);
+
 
         glm::mat4 model = glm::mat4(1.0f);
 
